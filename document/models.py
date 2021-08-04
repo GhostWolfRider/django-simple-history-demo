327c7ba802f7a6a96b44faaa073a3fbfce037c32
Krunal Rajkotiya <krunal@auberginesolutions.com>
2021-08-04 18:54:47 +0530

    added: document & users app

---
+++ document/models.py
@@ -0,0 +1,40 @@
+import uuid
+from django.contrib.auth.models import User
+from django.db import models
+
+try:
+    from django.db.models import JSONField
+except ImportError:
+    from django.contrib.postgres.fields import JSONField
+
+
+class Document(models.Model):
+    # UUID of the document
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    # name of the document
+    title = models.CharField(max_length=128)
+
+    # JSON value of the entire Abstract
+    abstract_json = JSONField()
+
+    # True if document is moved to archives
+    is_archive = models.BooleanField(default=False)
+
+    # True when document is moved to Trash
+    in_trash = models.BooleanField(default=False)
+
+    # creator of the document
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='document_created_by')
+
+    # timestamp for when the document was created
+    created_ts = models.DateTimeField(auto_now_add=True)
+
+    # user who modifies the document
+    modified_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='document_modified_by')
+
+    # timestamp for when the document was last modified
+    modified_ts = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
---
+++ users/models.py
@@ -0,0 +1,39 @@
+import uuid
+from django.contrib.auth.models import User
+from django.db import models
+
+
+# Create your models here.
+class Profile(models.Model):
+    """
+    Extending the User model to create extra fields.
+    """
+    # UUID of profile
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    # A user shall have only one profile and a profile shall represent only one user
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    # User profile image
+    image = models.CharField(max_length=1024, blank=True, null=True)
+
+    # User Image Url from s3
+    image_url = models.CharField(max_length=1024, blank=True, null=True)
+
+    # User mobile number
+    mobile_number = models.CharField(max_length=15, null=True, blank=True)
+
+    # Soft delete a user
+    is_deleted = models.BooleanField(default=False)
+
+    # User can be created by Admin.
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='profile_created_by')
+
+    # Timestamp for when the profile was created
+    created_ts = models.DateTimeField(auto_now_add=True)
+
+    # user who modifies the document
+    modified_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='profile_modified_by')
+
+    # Timestamp for when the profile was last modified
+    modified_ts = models.DateTimeField(auto_now_add=True)
